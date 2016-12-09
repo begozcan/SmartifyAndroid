f014011726042ce37d08613cc5a827fafead7d82
Begum Ozcan <begumozcan95@gmail.com>
2016-12-09 12:32:35 +0300

    Added swipe menu to generic ListView

--- app/src/main/java/company/whitespace/smartifyandroid/other/DevicesViewAdapter.java
+++ app/src/main/java/company/whitespace/smartifyandroid/other/DevicesViewAdapter.java
@@ -1,11 +1,13 @@
 package company.whitespace.smartifyandroid.other;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.tubb.smrv.SwipeHorizontalMenuLayout;
 import company.whitespace.smartifyandroid.R;
 import company.whitespace.smartifyandroid.fragment.DeviceFragment.OnListFragmentInteractionListener;
 import company.whitespace.smartifyandroid.fragment.dummy.DummyContent.DummyItem;
@@ -30,15 +32,15 @@ public class DevicesViewAdapter extends RecyclerView.Adapter<DevicesViewAdapter.
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_device, parent, false);
+                .inflate(R.layout.swipe_menu, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).id);
-        holder.mContentView.setText(mValues.get(position).content);
+        holder.itemID.setText(mValues.get(position).id);
+        holder.itemName.setText(mValues.get(position).content);
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +52,33 @@ public class DevicesViewAdapter extends RecyclerView.Adapter<DevicesViewAdapter.
                 }
             }
         });
+
+        holder.btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SWIPE MENU", "Open");
+            }
+        });
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                // must close normal
+//                myViewHolder.sml.smoothCloseMenu();
+//                users.remove(vh.getAdapterPosition());
+//                mAdapter.notifyItemRemoved(vh.getAdapterPosition());
+                Log.d("SWIPE MENU", "Delete");
+            }
+        });
+
+        holder.btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SWIPE MENU", "Left");
+            }
+        });
+
+
+        holder.sml.setSwipeEnable(true);
     }
 
     @Override
@@ -59,20 +88,32 @@ public class DevicesViewAdapter extends RecyclerView.Adapter<DevicesViewAdapter.
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public final TextView mIdView;
-        public final TextView mContentView;
         public DummyItem mItem;
 
-        public ViewHolder(View view) {
-            super(view);
-            mView = view;
-            mIdView = (TextView) view.findViewById(R.id.id);
-            mContentView = (TextView) view.findViewById(R.id.content);
+        TextView itemID;
+        TextView itemName;
+
+        View btnOpen;
+        View btnDelete;
+        View btnLeft;
+        SwipeHorizontalMenuLayout sml;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            itemID = (TextView) itemView.findViewById(R.id.id);
+            itemName = (TextView) itemView.findViewById(R.id.content);
+
+            btnOpen = itemView.findViewById(R.id.btOpen);
+            btnDelete = itemView.findViewById(R.id.btDelete);
+            btnLeft = itemView.findViewById(R.id.btLeft);
+            sml = (SwipeHorizontalMenuLayout) itemView.findViewById(R.id.sml);
         }
 
         @Override
         public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+            return super.toString() + " '" + itemName.getText() + "'";
         }
+
     }
 }
